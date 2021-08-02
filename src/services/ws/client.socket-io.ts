0b4c6ee549643a63c5c7c7fb13e57dcf3cde7538
Maik Laschober <maik.laschober@omega-center.com>
2021-08-02 12:14:07 +0200

    added: websocket service (basics)

---
+++ src/services/ws/client.socket-io.ts
@@ -0,0 +1,46 @@
+import io from 'socket.io-client';
+
+const isDev = localStorage.getItem('isDev') == '1';
+
+const socket = io(process.env.VUE_APP_WS_URI, {
+	withCredentials: true,
+});
+
+socket.on('connect', () => {
+	if (isDev) console.log(`SocketIO connected: ${process.env.VUE_APP_WS_URI}`);
+});
+
+socket.on('connect_error', (err) => {
+	const { message } = err;
+	if (message === 'Invalid authorization') {
+		const rawUser = localStorage.getItem('user') || '';
+		const userData = JSON.parse(rawUser);
+		if (isDev) console.log('connect_error', { err, rawUser, userData });
+		if (userData) {
+			socket.auth = { token: userData.token };
+			socket.connect();
+		}
+		setTimeout(() => {
+			socket.connect();
+		}, 2000);
+	}
+});
+
+// .disconnect is called on logout, so we manually connect again.
+socket.on('disconnect', () => {
+	socket.auth = {};
+	setTimeout(() => {
+		socket.connect();
+	}, 2000);
+});
+
+if (isDev) {
+	socket.on('connect_timeout', (a) => {
+		console.log('connect_timeout', a);
+	});
+	socket.on('reconnect', (a) => {
+		console.log('reconnect', a);
+	});
+}
+
+export default socket;
---
+++ src/services/ws/index.ts
@@ -0,0 +1,5 @@
+import client from './client.socket-io';
+
+export default {
+	client,
+};
